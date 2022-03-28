6d1f5d3292f83edce2db26e0804a409d0465712f
Vanya-Madatov <81025053+Vanya-Madatov@users.noreply.github.com>
2022-03-28 20:12:29 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $(".slider").owlCarousel({
+        items: 3,
+        smartSpeed: 1000,
+        autoplayHoverPause: true,
+        slideTransition: 'linear'
+    });
+
+
+});
